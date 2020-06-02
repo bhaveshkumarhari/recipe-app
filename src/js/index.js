12c84135929005cc2e7e1ec2b87d4f1b1d496607
bhaveshkumarhari <bhaveshkumar.hari@gmail.com>
2020-06-02 21:42:22 +0100

    Making API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-// Global app controller
-import num from './test';
-const x =23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module...! Varialbe x is ${x}`);
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+    
+}
+getResults('pizza');
